2e462dcdbd0ff751bef8de51884a1bb515154093
hassan-boulhilt <hboulhilt@gmail.com>
2022-07-20 21:52:01 +0100

    add toast and install it wit npm

--- ecom_vue/src/views/Product.vue
+++ ecom_vue/src/views/Product.vue
@@ -37,6 +37,7 @@
 </template>
 <script>
 import axios from 'axios'
+import {toast} from 'bulma-toast'
 export default {
     name:'Product',
     data(){
@@ -73,6 +74,15 @@ export default {
                 quantity: this.quantity
             }
             this.$store.commit('addToCart', item)
+
+            toast({
+                message: 'The product was added to the cart',
+                type: 'is-success',
+                dismissible : true,
+                pauseOnHover: true,
+                duration: 2000,
+                position:'bottom-right',
+            })
         },
         
 
